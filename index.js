eaa1fba6481b8b23b1bb6852f75b6a78264b941c
Aydin Gursoy <aydin_gurs@hotmail.com>
2023-12-11 16:39:11 +0000

    Basic JS working for charAt method

---
+++ index.js
@@ -0,0 +1,10 @@
+const charAtNumber = document.querySelector('#charAtNumber');
+const charAtVariableContent = document.querySelector('#charAtVariableContent');
+const charAtOutputResult = document.querySelector('#charAtOutputResult');
+const charAtButton = document.querySelector('#charAtButton');
+
+charAtButton.addEventListener('click', function() {
+    const charAtVariableValue = charAtVariableContent.value;
+    const charAtNumberValue = parseInt(charAtNumber.value);
+    charAtOutputResult.textContent = charAtVariableValue.charAt(charAtNumberValue);
+});
