a87e70530d1a23d7942fcd16be68e12c8c19fa54
Aydin Gursoy <aydin_gurs@hotmail.com>
2023-12-12 13:25:09 +0000

    Change charAt description and add error handling to charAt inputs.

--- index.js
+++ index.js
@@ -5,6 +5,35 @@ const charAtButton = document.querySelector('#charAtButton');
 
 charAtButton.addEventListener('click', function() {
     const charAtVariableValue = charAtVariableContent.value;
-    const charAtNumberValue = parseInt(charAtNumber.value);
-    charAtOutputResult.textContent = charAtVariableValue.charAt(charAtNumberValue);
+    const charAtNumberValue = Number(charAtNumber.value);
+
+    if(!/\S/.test(charAtVariableValue) === true) {
+        charAtOutputResult.textContent = `This method doesn't work without a word! Error found!`
+        charAtOutputResult.style.height = 'auto'
+    }
+
+    else if(charAtNumberValue < 0) {
+        charAtOutputResult.textContent = `Positive numbers only! Error found!`
+        charAtOutputResult.style.height = 'auto'
+    }
+
+    else if(!Number.isInteger(charAtNumberValue)) {
+        charAtOutputResult.textContent = `It's ${charAtVariableValue.charAt(charAtNumberValue)}, but it ignores your decimal and uses the integer portion only. Error found!`;
+        charAtOutputResult.style.height = 'auto'
+        console.log(charAtNumberValue)
+    }
+
+    else if((charAtNumberValue+1) > charAtVariableValue.length) {
+        charAtOutputResult.textContent = `There isn't that many characters! Error found!`
+        charAtOutputResult.style.height = 'auto'
+    }
+
+    else if(!Number(charAtNumberValue)) {
+        charAtOutputResult.textContent = `indexNumber needs to be a number, or if no number is input, it will make it 0 by default. Error found!`
+        charAtOutputResult.style.height = 'auto'
+    }
+    else {
+        charAtOutputResult.textContent = charAtVariableValue.charAt(charAtNumberValue);
+        console.log(charAtNumberValue)
+    }
 });
